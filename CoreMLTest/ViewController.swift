d27c874438684d7baff25182c114bd2bea6d1888
Ilya Sysoi <14ilya12322@gmail.com>
2018-11-19 09:43:16 +0300

    project init

--- CoreMLTest/ViewController.swift
+++ CoreMLTest/ViewController.swift
@@ -7,14 +7,108 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var resultView: UIView!
+    
+    let session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    let captureQueue = DispatchQueue(label: "captureQueue")
+    var visionRequests = [VNRequest]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCamera()
     }
-
-
+    
+    private func setupCamera() {
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        do {
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            resultView.layer.addSublayer(previewLayer)
+            
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            let videoOutput = AVCaptureVideoDataOutput()
+            videoOutput.setSampleBufferDelegate(self,
+                                                queue: captureQueue)
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+            
+            session.sessionPreset = .high
+            session.addInput(cameraInput)
+            session.addOutput(videoOutput)
+            
+            let connection = videoOutput.connection(with: .video)
+            connection?.videoOrientation = .portrait
+            session.startRunning()
+            
+            guard let visionModel = try? VNCoreMLModel(for: Inceptionv3().model) else {
+                fatalError("Could not load model")
+            }
+            
+            let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+            classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+            visionRequests = [classificationRequest]
+        } catch {
+            let alertController = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.frame
+    }
+    
+    private func handleClassifications(request: VNRequest, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        guard let results = request.results as? [VNClassificationObservation] else {
+            print("No results")
+            return
+        }
+        
+        var resultString = "Это не кот!"
+        results[0...3].forEach {
+            let identifer = $0.identifier.lowercased()
+            if identifer.range(of: " cat") != nil || identifer.range(of: "cat ") != nil || identifer == "cat" {
+                resultString = "Это кот!"
+            }
+        }
+        DispatchQueue.main.async {
+            self.resultLabel.text = resultString
+        }
+    }
 }
 
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        var requestOptions: [VNImageOption: Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics: cameraIntrinsicData]
+        }
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation.up, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(visionRequests)
+        } catch {
+            print(error)
+        }
+    }
+}
